c615d19427809baad32696eb988d27584368fd1e
mdanieli <mdanieli@outlook.com>
2022-07-03 16:04:42 -0300

    Modificamos index por palabra home

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index, crear_noticia, listado_noticias, nosotros
+from .views import home, crear_noticia, listado_noticias, nosotros
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', home, name='home'),
     path('noticias/', listado_noticias, name='listado_noticias'),
     path('crear-noticia/', crear_noticia, name='crear_noticia'),
     path('nosotros/', nosotros, name='nosotros')
